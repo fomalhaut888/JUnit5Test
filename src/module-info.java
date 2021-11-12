7892dcff36fced129e84f3bab15ffd2f21c6a202
SimonC <SimonC@leosys.com>
2021-11-12 16:21:06 +0800

    test

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,7 @@
 module junit5_test {
 	requires org.junit.jupiter.api;
 	requires org.junit.jupiter.params;
-	requires org.junit.platform.engine;
+	//requires org.junit.jupiter.api;
+	//requires org.junit.jupiter.params;
+	//requires org.junit.platform.engine;
 }
